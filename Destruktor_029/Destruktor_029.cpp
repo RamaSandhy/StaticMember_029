781b4cd2da0fe2fbdc4aeed33b6bd370f0a3702f
RamaSandhy <rshandy07@gmail.com>
2023-05-17 16:43:50 +0700

    memasukkan class angka private dan public

--- Destruktor_029/Destruktor_029.cpp
+++ Destruktor_029/Destruktor_029.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 using namespace std;
 
+class angka {
+private:
+	int* arr;
+	int panjang;
+public:
+	angka(int); //Constructor wangka(); //Destructor void cetakData ();
+	~angka(); //Destructor
+	void cetakData();
+	void isiData();
+};
